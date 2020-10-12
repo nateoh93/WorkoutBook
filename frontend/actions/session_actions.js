4f1d660d6224936424e1e55e43fc7f759c465d8d
nateoh93 <nathanoh93@gmail.com>
2020-10-12 14:18:19 -0700

    session actions and reducers set up

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,38 +4,39 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-export const receiveCurrentUser = currentUser => ({
+const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
     currentUser
 });
 
-export const logoutCurrentUser = () => ({
+const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER,
 });
 
-export const receiveErrors = errors => ({
+const receiveErrors = errors => ({
     type: RECEIVE_SESSION_ERRORS,
     errors
 });
 
-export const signup = user => dispatch => (
-    APIUtil.signup(user).then(user => (
-        dispatch(receiveCurrentUser(user))
-    ), err => (
-        dispatch(receiveErrors(err.responseJSON))
-    ))
-);
+export const signup = formUser => {
+    return (dispatch) => {
+        return APIUtil.signup(formUser)
+            .then(user => { dispatch(receiveCurrentUser(user)) })
+            .fail(error => { dispatch(receiveErrors(error.responseJSON)) })
+    }
+}
 
-export const login = user => dispatch => (
-    APIUtil.login(user).then(user => (
-        dispatch(receiveCurrentUser(user))
-    ), err => (
-        dispatch(receiveErrors(err.responseJSON))
-    ))
-);
+export const login = formUser => {
+    return (dispatch) => {
+        return APIUtil.login(formUser)
+            .then(user => { dispatch(receiveCurrentUser(user)) })
+            .fail(error => { dispatch(receiveErrors(error.responseJSON))})
+    }
+}
 
-export const logout = () => dispatch => (
-    APIUtil.logout().then(() => (
-        dispatch(logoutCurrentUser())
-    ))
-);
+export const logout = () => {
+    return (dispatch) => {
+        return APIUtil.logout()
+            .then(() => { dispatch(logoutCurrentUser()) })
+    }
+}
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
 
 //testing start
 import * as SessionAPIUtil from './util/session_api_util'
+import {signup, login, logout} from './actions/session_actions'
 //testing end
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Workoutbook</h1>, root)
 
     //testing start
-    window.signup = SessionAPIUtil.signup
-    window.login = SessionAPIUtil.login
-    window.logout = SessionAPIUtil.logout
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
     //testing end
 })
