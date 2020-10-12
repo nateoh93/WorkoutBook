8962070796e41f6171d4f3bcf3de61e3a00000d5
nateoh93 <nathanoh93@gmail.com>
2020-10-12 00:51:22 -0700

    Update user routes and show view

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,51 @@
 class Api::UsersController < ApplicationController
+    # before_action :require_logged_in, except: [:create]
+    
+    def index
+        @users = User.all
+        render :show
+        # include a render
+    end
+    
+    def show
+        @user = User.find_by(id: params[:id])
+        # include a render
+    end
+    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+    
+    def update
+        @user = User.find_by(id: params[:id])
+        
+        if @user.update(user_params)
+            # redirect somewhere
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :email, 
+            :password, 
+            :fname, 
+            :lname, 
+            :birthday, 
+            :bio, 
+            :city,
+            :work,
+            :school
+        )
+    end
 end
---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :email, :fname, :lname, :birthday, :bio, :city, :work, :school
