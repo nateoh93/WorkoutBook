fda568844336a33b60fbe3be60ec1d06606080fd
nateoh93 <nathanoh93@gmail.com>
2020-10-13 16:08:30 -0700

    Prepare for initial Heroku push

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root'
 
-//TESTING START
-import * as SessionAPIUtil from './util/session_api_util'
-import {signup, login, logout} from './actions/session_actions'
-//TESTING END
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
@@ -27,11 +23,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store}/>, root)
 
-    //TESTING START
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
-    //TESTING END
 })
