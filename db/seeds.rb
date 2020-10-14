5d1798b454c40aa4986cd32951799a45e739259a
nateoh93 <nathanoh93@gmail.com>
2020-10-13 21:19:10 -0700

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,56 @@
 User.destroy_all
 
 user1 = User.create!(
+    email: 'demo_user@squat.com',
+    password: 'password',
+    fname: 'demo',
+    lname: 'user',
+    birthday: '2000/01/01',
+    bio: "Running is the best form of exercise.",
+    city: "San Francisco",
+    school: "Marathon University",
+)
+
+user2 = User.create!(
+    email: 'alex@squat.com',
+    password: 'password',
+    fname: 'alex',
+    lname: 'yudit',
+    birthday: '2000/02/01',
+    bio: "I like to eat!",
+    city: "San Francisco",
+    school: "Snack University",
+)
+
+user3 = User.create!(
+    email: 'ken@squat.com',
+    password: 'password',
+    fname: 'ken',
+    lname: 'ashe',
+    birthday: '2000/03/01',
+    bio: "I like to do burpees!",
+    city: "San Francisco",
+    school: "Burpee University",
+)
+
+user4 = User.create!(
     email: 'nate@squat.com',
     password: 'password',
     fname: 'nate',
     lname: 'oh',
-    birthday: '2000/03/01'
+    birthday: '2000/04/01',
+    bio: "I like to squat!",
+    city: "San Francisco",
+    school: "Squat University",
+)
+
+user5 = User.create!(
+    email: 'theo@squat.com',
+    password: 'password',
+    fname: 'theo',
+    lname: 'oh',
+    birthday: '2000/05/01',
+    bio: "I only workout my arms!",
+    city: "San Francisco",
+    school: "Curl University",
 )
