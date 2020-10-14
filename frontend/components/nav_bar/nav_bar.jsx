cb3f3e33ef1befd5b524beb1473da06d8ad7473a
nateoh93 <nathanoh93@gmail.com>
2020-10-14 02:13:55 -0700

    Navbar styling

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -10,9 +10,25 @@ class NavBar extends React.Component{
         return (
             <div className='navbar'>
                 <nav>
-                    <Link to='/feed'><img src={window.logo}/></Link>
-                    <Link to='/feed'>Home</Link>
-                    {this.props.currentUser ? <button className="header-button" onClick={this.props.logout}>Log Out</button> : null}
+                    <div className='navbar-left'>
+                        <div className='nav-logo'><Link to='/feed'><img src={window.logo} /></Link></div>
+                    </div>
+
+                    <div className='navbar-right'>
+                        <Link to='/feed'>Home</Link>
+                        <div className='nav-icon-list'>
+                            <div className='friend-request-icon'></div>
+                            <div className='messenger-icon'></div>
+                            <div className='notifications-icon'></div>
+                            <div className='logout-icon'>
+                                <div className='logout-notifications'>
+                                    <li>
+                                        {this.props.currentUser ? <button className="header-button" onClick={this.props.logout}>Logout</button> : null}
+                                    </li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </nav>
             </div>
         )
