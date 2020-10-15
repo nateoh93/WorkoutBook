ae03d2b6da94e827de77b3b769f3c8fa0dcc8f96
nateoh93 <nathanoh93@gmail.com>
2020-10-14 19:26:33 -0700

    Profile header setup

---
+++ frontend/components/profile/profile_header_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import ProfileHeader from './profile';
+
+const mapStateToProps = (state) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    // return ({null})
+});
+
+export default connect(
+    mapStateToProps,
+    null
+)(ProfileHeader);
