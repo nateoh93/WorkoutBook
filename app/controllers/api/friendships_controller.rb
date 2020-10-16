229b636454c0e260809758c9a76622821d0a1566
nateoh93 <nathanoh93@gmail.com>
2020-10-16 14:21:50 -0700

    Create friendship controller

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -1,2 +1,31 @@
 class Api::FriendshipsController < ApplicationController
+    def create
+        @friend = Friend.new(user_id: current_user.id, friend_id: params[:id])
+        @inverse_friend = Friend.new(user_id: params[:id], friend_id: current_user.id)
+
+        if Friend.find_by(user_id: current_user.id, friend_id: params[:id])
+            render json: ["Already friends, can't add friend again."], status: 422
+        elsif @friend.save && @inverse_friend.save
+            render :show
+        else
+            render json: @friend.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @friend = Friend.find_by(user_id: current_user.id, friend_id: params[:id])
+        @inverse_friend = Friend.find_by(user_id: params[:id], friend_id: current_user.id)
+
+        if @friend.destroy && @inverse_friend.destroy
+            render json: {}
+        else
+            render json: @friend.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def friend_params
+        params.require(:friendships).permit(:user_id, :friend_id)
+    end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,7 @@
 class Friendship < ApplicationRecord
     validates :user1_id, :user2_id, presence: true
     validates :user1_id, uniqueness: { scope: :user2_id}
-    validates :no_self_add
+    validates :reject_self_add
 
     belongs_to :user,
         foreign_key: :user1_id,
@@ -11,21 +11,7 @@ class Friendship < ApplicationRecord
         foreign_key: :user2_id,
         class_name: :User
 
-    after_create: :create_opposite_friendship
-    after_destroy: :destroy_opposite_friendship
-
-    def create_opposite_friendship
-        Friendship.create!(user1_id: friend.id, user2_id: user.id)
-    end
-
-    def destroy_opposite_friendship
-        opposite_relationship = friend.friendships.find_by(user2_id: user.id)
-        if opposite_relationship
-            opposite_relationship.destroy
-        end
-    end
-
-    def no_self_add
+    def reject_self_add
         if user1_id == user2_id
             errors[:user1_id] << "You can't add yourself"
         end
