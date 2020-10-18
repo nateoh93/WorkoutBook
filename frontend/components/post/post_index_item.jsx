f76f19d72c5e2c55102f9876138fcb9e853a78c0
nateoh93 <nathanoh93@gmail.com>
2020-10-17 18:11:13 -0700

    Post styling

--- frontend/components/post/post_index_item.jsx
+++ frontend/components/post/post_index_item.jsx
@@ -11,8 +11,8 @@ class PostIndexItem extends React.Component{
                 <div>
                     <button className='post-menu-btn-icon'></button>
                     <ul className='post-menu-dropdown-list'>
-                        <li>Edit</li>
-                        <li>Delete</li>
+                        <li><button className='post-menu-dropdown-btn'>Edit</button></li>
+                        <li><button className='post-menu-dropdown-btn'>Delete</button></li>
                     </ul>
                 </div>
                 <div className='wallpost-body'>{this.props.post.body}</div>
