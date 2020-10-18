a6270ccb9f331dc7cd60d59ac5f2753261104a30
nateoh93 <nathanoh93@gmail.com>
2020-10-17 23:57:37 -0700

    Edit post modal styling

--- frontend/components/post/edit_post_form.jsx
+++ frontend/components/post/edit_post_form.jsx
@@ -34,7 +34,7 @@ class EditPostForm extends React.Component {
     render() {
         return (
             <form className='update-post-form' onSubmit={this.handleSubmit}>
-                <span onClick={this.props.closeModal} className="close-x update-form">&times;</span>
+                <span onClick={this.props.closeModal} className="close-x post-form-modal">&times;</span>
                 <h3>Edit Post </h3>
                 <textarea  onChange={this.update('body')} value={this.state.body}></textarea>
                 <button>Save</button>
