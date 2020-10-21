1e417a76e791dbd9339af3774bc60ffe71e0e46f
nateoh93 <nathanoh93@gmail.com>
2020-10-20 21:25:51 -0700

    Newsfeed links styling

--- frontend/components/news_feed/news_feed_link.jsx
+++ frontend/components/news_feed/news_feed_link.jsx
@@ -13,17 +13,28 @@ class NewsFeedLink extends React.Component {
                 <div className='feed-profile-photo'>
                     <Link to={`/users/${this.props.currentUser.id}`}>
                         <img className='feed-pic' src={this.props.currentUser.profilePhoto} />
-                        {this.props.currentUser.fname} {this.props.currentUser.lname}
+                        <div className='newsfeed-name'>{this.props.currentUser.fname} {this.props.currentUser.lname}</div>
                     </Link>
                 </div>
 
                 <li className='feed-home-button'><Link to='/feed'>News Feed</Link></li>
 
-                <div className='newsfeed-outside-links'>Shortcuts
-                    <a href="https://www.linkedin.com/in/nathan-oh-cpa-8062075a/"><i class="fab fa-linkedin"></i>LinkedIn</a>
-                    <a href="https://github.com/nateoh93"><i class="fab fa-github"></i>GitHub</a>
-                    <a href="https://github.com/nateoh93/WorkoutBook"><i class="fas fa-code-branch"></i>Workoutbook Repo</a>
-                    
+                <div className='newsfeed-outside-links'>
+                    <h1>Shortcuts</h1>
+                    <a href="https://www.linkedin.com/in/nathan-oh-cpa-8062075a/"><div className='link-container'>
+                        <div className='link-name'>LinkedIn</div>
+                        <i className="fab fa-linkedin link-icon"></i>
+                    </div></a>
+
+                    <a href="https://github.com/nateoh93"><div className='link-container'>
+                        <div className='link-name'>GitHub</div>
+                        <i className="fab fa-github link-icon"></i>
+                    </div></a>
+
+                    <a href="https://github.com/nateoh93/WorkoutBook"><div className='link-container'>
+                        <div className='link-name'>Workoutbook Repo</div>
+                        <i className="fas fa-code-branch link-icon"></i>
+                    </div></a>
                 </div>
 
             </div>
