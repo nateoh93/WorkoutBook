f98907b5fffabc3ed4221824be2a25c104b7d0c3
nateoh93 <nathanoh93@gmail.com>
2020-11-10 18:50:13 -0800

    add request icon

--- frontend/components/friend/friend_form.jsx
+++ frontend/components/friend/friend_form.jsx
@@ -11,9 +11,8 @@ class FriendForm extends React.Component {
 
     handleRequest(e) {
         e.preventDefault();
-
-        this.props.requestFriend({ requestee_id: this.props.postProfile.id }).then(this.props.fetchAllUsers())
-    }
+        this.props.requestFriend({ requestee_id: this.props.postProfile.id }).then(this.props.fetchAllUsers());
+    };
     
     handleRemove(e) {
         e.preventDefault();
@@ -21,28 +20,27 @@ class FriendForm extends React.Component {
         this.props.currentUserFriends.forEach( friend => {
             if (friend.friend_id === this.props.postProfile.id) {
                 friendship = friend;
-            }
-        })
-        this.props.deleteFriend(friendship).then(this.props.fetchUser(this.props.postProfile.id))
+            };
+        });
+        this.props.deleteFriend(friendship).then(this.props.fetchUser(this.props.postProfile.id));
     }
 
     displayFriendButton() {
-        
         let alreadyFriends = false;
         let pendingFriendship = false;
 
         this.props.currentUserFriends.forEach( friend => {
             if (friend.friend_id === this.props.postProfile.id) {
                 alreadyFriends = true;
-            }
-        })
+            };
+        });
 
         this.props.friendRequests.forEach( request => {
             if ((request.requester_id === this.props.postProfile.id && request.requestee_id === this.props.currentUser.id) ||
                 (request.requestee_id === this.props.postProfile.id && request.requester_id === this.props.currentUser.id)) {
                     pendingFriendship = true;
-            }
-        })
+            };
+        });
         
         if (this.props.postProfile.id === this.props.currentUser.id) {
             return null;
@@ -52,9 +50,8 @@ class FriendForm extends React.Component {
             return <button className='pending-friendship-btn'><i class="fas fa-user-edit"></i>Pending</button>
         } else {
             return <button className='request-friendship-btn' onClick={this.handleRequest}><i class="fas fa-user-plus"></i>Add Friend</button>
-        }
-
-    }
+        };
+    };
 
     render() {
         return (
